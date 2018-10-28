6fb95ea396618fd14591167150e9c1de17eb4926
Morgan Brown <brown.mhg+git@gmail.com>
2018-10-28 20:09:30 +0100

    Add nav links

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,6 +2,14 @@
   <div class="home">
     <div class="content-container">
       <h1>Morgan Brown is a Software Engineer based in Berlin, Germany</h1>
+      <ul>
+        <li><a target="_blank" href="http://github.com/mhgbrown">Code</a></li>
+        <li><a target="_blank" href="http://instagram.com/mhgbrown">Photos</a></li>
+        <li><a target="_blank" href="http://soundcloud.com/mhgbrown/likes">Music</a></li>
+        <li><a target="_blank" href="http://blog.mhgbrown.is">Writing</a></li>
+        <li><a target="_blank" href="http://www.linkedin.com/in/mhgbrown">Resume</a></li>
+        <li><a target="_blank" href="mailto:inbox@email.mhgbrown.is">Contact</a></li>
+      </ul>
     </div>
     <slide-show></slide-show>
     <div class="content-container">
