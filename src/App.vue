177aaf886e5dc1c669ec87ddafdf69ebb397750d
Morgan Brown <git@email.mhgbrown.is>
2026-05-23 12:07:51 +0300

    refactor: remove unused resize listener and window dimensions store from App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,25 +7,6 @@
 
 <script setup lang="ts">
 import SeoMeta from '@/components/seo-meta.vue'
-import { onMounted, onUnmounted } from 'vue'
-import { useResumeStore } from '@/store.ts'
-
-const store = useResumeStore()
-
-const onResize = () => {
-  const windowWidth = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth
-  store.updateApp('tablet', windowWidth <= 960)
-  store.updateApp('phone', windowWidth <= 480)
-}
-
-onMounted(() => {
-  window.addEventListener('resize', onResize, true)
-  onResize()
-})
-
-onUnmounted(() => {
-  window.removeEventListener('resize', onResize, true)
-})
 </script>
 
 <style lang="scss">
